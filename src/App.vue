b5d350dc65892311d50723f19707988724370177
junhyuk <49514381+jnhk123@users.noreply.github.com>
2022-10-22 17:26:17 +0900

    slot

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,55 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div>
+      <button @click="firstModal = true">첫번째 모달</button>
+    </div>
+    <hr />
+    <div>
+      <button @click="secondModal = true">두번째 모달</button>
+    </div>
+
+    <ModalView :on="firstModal" @close="firstModal = false">
+      <template v-slot:title>제목입니다.</template>
+      <template #sub-title>부제목입니다.</template>
+      <template>
+        <div>
+          <p>시작입니다.</p>
+          <input type="text" name="" id="" value="안녕하세요" />
+          <p>끝!!</p>
+        </div>
+      </template>
+    </ModalView>
+
+    <ModalView :on="secondModal" @close="secondModal = false">
+      <template v-slot:title>오예</template>
+      <template #sub-title>반갑습니다.</template>
+      <template>
+        <button @click="click">클릭하세요!!</button>
+      </template>
+    </ModalView>
+  </div>
 </template>
 
 <script>
+import ModalView from "./components/ModalView.vue";
+
 export default {
-  components: {},
+  data() {
+    return {
+      firstModal: false,
+      secondModal: false,
+    };
+  },
+
+  components: {
+    ModalView,
+  },
+
+  methods: {
+    click() {
+      alert("클릭되었습니다.!!!");
+    },
+  },
 };
 </script>
 
---
+++ src/components/ModalView.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container" v-if="on">
+    <div class="popup-wrap" id="popup">
+      <div class="popup">
+        <div class="popup-head">
+          <span class="head-title">
+            <slot name="title"></slot>
+          </span>
+          <span @click="close">닫기</span>
+        </div>
+        <div class="popup-body">
+          <div class="body-content">
+            <div class="body-titlebox">
+              <h1>
+                <slot name="sub-title"></slot>
+              </h1>
+            </div>
+            <div class="body-contentbox">
+              <p>
+                <slot name="default"></slot>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    on: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data: () => ({}),
+
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.popup-wrap {
+  background-color: rgba(0, 0, 0, 0.3);
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: none;
+  display: flex;
+  padding: 15px;
+}
+
+.popup {
+  width: 100%;
+  max-width: 400px;
+  border-radius: 10px;
+  overflow: hidden;
+  background-color: #264db5;
+  box-shadow: 5px 10px 10px 1px rgba(0, 0, 0, 0.3);
+}
+
+.popup-head {
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.head-title {
+  color: white;
+}
+
+.popup-body {
+  width: 100%;
+  background-color: #ffffff;
+}
+
+.body-content {
+  padding: 30px;
+}
+
+.body-titlebox {
+  text-align: center;
+  width: 100%;
+  height: 40px;
+  margin-bottom: 10px;
+}
+.body-contentbox {
+  word-break: break-word;
+  overflow-y: auto;
+  min-height: 100px;
+  max-height: 200px;
+}
+</style>
